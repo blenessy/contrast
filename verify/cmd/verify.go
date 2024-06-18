017a6809dca0530446a0b1c69be3b03164b58ed7
Zalan Blenessy <zalan.blenessy@gmail.com>
2024-06-18 12:13:12 +0200

    Break out the verify subcommand into its own binary

---
+++ verify/cmd/verify.go
@@ -0,0 +1,236 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package cmd
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+
+	"github.com/edgelesssys/contrast/internal/atls"
+	"github.com/edgelesssys/contrast/internal/attestation/snp"
+	"github.com/edgelesssys/contrast/internal/fsstore"
+	"github.com/edgelesssys/contrast/internal/grpc/dialer"
+	"github.com/edgelesssys/contrast/internal/manifest"
+	"github.com/edgelesssys/contrast/internal/userapi"
+	"github.com/google/go-sev-guest/abi"
+	"github.com/google/go-sev-guest/kds"
+	"github.com/google/go-sev-guest/validate"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+const (
+	cacheDirEnv          = "CONTRAST_CACHE_DIR"
+	coordRootPEMFilename = "coordinator-root-ca.pem"
+	meshCAPEMFilename    = "mesh-ca.pem"
+	verifyDir            = "./verify"
+)
+
+var DefaultCoordinatorPolicyHash = "";
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func cachedir(subdir string) (string, error) {
+	dir := os.Getenv(cacheDirEnv)
+	if dir == "" {
+		cachedir, err := os.UserCacheDir()
+		if err != nil {
+			return "", err
+		}
+		dir = filepath.Join(cachedir, "contrast")
+	}
+	return filepath.Join(dir, subdir), nil
+}
+
+
+func decodeCoordinatorPolicyHash(flags *pflag.FlagSet) ([]byte, error) {
+	hexEncoded, err := flags.GetString("coordinator-policy-hash")
+	if err != nil {
+		return nil, fmt.Errorf("getting coordinator-policy-hash flag: %w", err)
+	}
+	hash, err := hex.DecodeString(hexEncoded)
+	if err != nil {
+		return nil, fmt.Errorf("hex-decoding coordinator-policy-hash flag: %w", err)
+	}
+	if len(hash) != 32 {
+		return nil, fmt.Errorf("coordinator-policy-hash must be exactly 32 hex-encoded bytes, got %d", len(hash))
+	}
+	return hash, nil
+}
+
+// NewVerifyCmd creates the contrast verify subcommand.
+func NewVerifyCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "verify",
+		Short: "Verify a contrast deployment",
+		Long: `Verify a contrast deployment.
+
+This will connect to the given Coordinator using aTLS. During the connection
+initialization, the remote attestation of the Coordinator CVM happens and
+the connection will only be successful if the Coordinator conforms with the
+reference values embedded into the CLI.
+
+After the connection is established, the CLI will request the manifest history,
+all policies, and the certificates of the Coordinator certificate authority.`,
+		//RunE: withTelemetry(runVerify),
+	}
+
+	// Override persistent workspace-dir flag with a default value.
+	cmd.Flags().String("workspace-dir", verifyDir, "directory to write files to, if not set explicitly to another location")
+	cmd.Flags().StringP("coordinator", "c", "", "endpoint the coordinator can be reached at")
+	must(cobra.MarkFlagRequired(cmd.Flags(), "coordinator"))
+	cmd.Flags().String("coordinator-policy-hash", DefaultCoordinatorPolicyHash, "override the expected policy hash of the coordinator")
+
+	return cmd
+}
+
+func runVerify(cmd *cobra.Command, _ []string) error {
+	flags, err := parseVerifyFlags(cmd)
+	if err != nil {
+		return fmt.Errorf("parsing flags: %w", err)
+	}
+
+	log, err := newCLILogger(cmd)
+	if err != nil {
+		return err
+	}
+	log.Debug("Starting verification")
+
+	kdsDir, err := cachedir("kds")
+	if err != nil {
+		return fmt.Errorf("getting cache dir: %w", err)
+	}
+	log.Debug("Using KDS cache dir", "dir", kdsDir)
+
+	validateOptsGen := newCoordinatorValidateOptsGen(flags.policy)
+	kdsCache := fsstore.New(kdsDir, log.WithGroup("kds-cache"))
+	kdsGetter := snp.NewCachedHTTPSGetter(kdsCache, snp.NeverGCTicker, log.WithGroup("kds-getter"))
+	validator := snp.NewValidator(validateOptsGen, kdsGetter, log.WithGroup("snp-validator"))
+	dialer := dialer.New(atls.NoIssuer, validator, &net.Dialer{})
+
+	log.Debug("Dialing coordinator", "endpoint", flags.coordinator)
+	conn, err := dialer.Dial(cmd.Context(), flags.coordinator)
+	if err != nil {
+		return fmt.Errorf("Error: failed to dial coordinator: %w", err)
+	}
+	defer conn.Close()
+
+	log.Debug("Getting manifest")
+	client := userapi.NewUserAPIClient(conn)
+	resp, err := client.GetManifests(cmd.Context(), &userapi.GetManifestsRequest{})
+	if err != nil {
+		return fmt.Errorf("failed to get manifest: %w", err)
+	}
+	log.Debug("Got response")
+
+	fmt.Fprintln(cmd.OutOrStdout(), "✔️ Successfully verified coordinator")
+
+	filelist := map[string][]byte{
+		coordRootPEMFilename: resp.RootCA,
+		meshCAPEMFilename:    resp.MeshCA,
+	}
+	for i, m := range resp.Manifests {
+		filelist[fmt.Sprintf("manifest.%d.json", i)] = m
+	}
+	for _, p := range resp.Policies {
+		sha256sum := sha256.Sum256(p)
+		pHash := manifest.NewHexString(sha256sum[:])
+		filelist[fmt.Sprintf("policy.%s.rego", pHash)] = p
+	}
+	if err := writeFilelist(flags.workspaceDir, filelist); err != nil {
+		return fmt.Errorf("writing filelist: %w", err)
+	}
+
+	fmt.Fprintf(cmd.OutOrStdout(), "✔️ Wrote Coordinator configuration and keys to %s\n", flags.workspaceDir)
+	fmt.Fprintln(cmd.OutOrStdout(), "  Please verify the manifest history and policies")
+
+	return nil
+}
+
+type verifyFlags struct {
+	coordinator  string
+	workspaceDir string
+	policy       []byte
+}
+
+func parseVerifyFlags(cmd *cobra.Command) (*verifyFlags, error) {
+	coordinator, err := cmd.Flags().GetString("coordinator")
+	if err != nil {
+		return nil, err
+	}
+	workspaceDir, err := cmd.Flags().GetString("workspace-dir")
+	if err != nil {
+		return nil, err
+	}
+	policy, err := decodeCoordinatorPolicyHash(cmd.Flags())
+	if err != nil {
+		return nil, err
+	}
+
+	return &verifyFlags{
+		coordinator:  coordinator,
+		workspaceDir: workspaceDir,
+		policy:       policy,
+	}, nil
+}
+
+func newCoordinatorValidateOptsGen(hostData []byte) *snp.StaticValidateOptsGenerator {
+	defaultManifest := manifest.Default()
+	trustedMeasurement, err := defaultManifest.ReferenceValues.TrustedMeasurement.Bytes()
+	if err != nil {
+		panic(err) // We are decoding known values, tests should catch any failure.
+	}
+	if trustedMeasurement == nil {
+		// This is required to prevent an empty measurement in the manifest from disabling the measurement check.
+		trustedMeasurement = make([]byte, 48)
+	}
+
+	return &snp.StaticValidateOptsGenerator{
+		Opts: &validate.Options{
+			HostData:    hostData,
+			Measurement: trustedMeasurement,
+			GuestPolicy: abi.SnpPolicy{
+				Debug: false,
+				SMT:   true,
+			},
+			VMPL: new(int), // VMPL0
+			MinimumTCB: kds.TCBParts{
+				BlSpl:    3,
+				TeeSpl:   0,
+				SnpSpl:   8,
+				UcodeSpl: 115,
+			},
+			MinimumLaunchTCB: kds.TCBParts{
+				BlSpl:    3,
+				TeeSpl:   0,
+				SnpSpl:   8,
+				UcodeSpl: 115,
+			},
+			PermitProvisionalFirmware: true,
+		},
+	}
+}
+
+func writeFilelist(dir string, filelist map[string][]byte) error {
+	if dir != "" {
+		if err := os.MkdirAll(dir, 0o755); err != nil {
+			return fmt.Errorf("creating directory %s: %w", dir, err)
+		}
+	}
+	for filename, contents := range filelist {
+		path := filepath.Join(dir, filename)
+		if err := os.WriteFile(path, contents, 0o644); err != nil {
+			return fmt.Errorf("writing %q: %w", path, err)
+		}
+	}
+	return nil
+}
---
+++ verify/main.go
@@ -0,0 +1,79 @@
+// Copyright 2024 Edgeless Systems GmbH
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/edgelesssys/contrast/verify/cmd"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	if err := execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func execute() error {
+	cmd := newRootCmd()
+	ctx, cancel := signalContext(context.Background(), os.Interrupt)
+	defer cancel()
+	return cmd.ExecuteContext(ctx)
+}
+
+var version = "0.0.0-dev"
+
+func newRootCmd() *cobra.Command {
+	root := &cobra.Command{
+		Short:            "contrast",
+		PersistentPreRun: preRunRoot,
+		Version:          version,
+	}
+	root.SetOut(os.Stdout)
+
+	root.PersistentFlags().String("log-level", "warn", "set logging level (debug, info, warn, error, or a number)")
+	root.PersistentFlags().String("workspace-dir", "", "directory to write files to, if not set explicitly to another location")
+
+	root.InitDefaultVersionFlag()
+	root.AddCommand(
+		cmd.NewVerifyCmd(),
+	)
+
+	return root
+}
+
+// signalContext returns a context that is canceled on the handed signal.
+// The signal isn't watched after its first occurrence. Call the cancel
+// function to ensure the internal goroutine is stopped and the signal isn't
+// watched any longer.
+func signalContext(ctx context.Context, sig os.Signal) (context.Context, context.CancelFunc) {
+	sigCtx, stop := signal.NotifyContext(ctx, sig)
+	done := make(chan struct{}, 1)
+	stopDone := make(chan struct{}, 1)
+
+	go func() {
+		defer func() { stopDone <- struct{}{} }()
+		defer stop()
+		select {
+		case <-sigCtx.Done():
+			fmt.Println("\rSignal caught. Press ctrl+c again to terminate the program immediately.")
+		case <-done:
+		}
+	}()
+
+	cancelFunc := func() {
+		done <- struct{}{}
+		<-stopDone
+	}
+
+	return sigCtx, cancelFunc
+}
+
+func preRunRoot(cmd *cobra.Command, _ []string) {
+	cmd.SilenceUsage = true
+}
